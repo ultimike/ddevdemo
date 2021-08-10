643ed112be7595bdc7e16beb875d22dcf739192b
Michael Anello <manello@gmail.com>
2021-08-10 22:10:35 +0200

    Disabled config readonly on Dev.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -808,7 +808,7 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
 include __DIR__ . "/settings.pantheon.php";
 
 if (isset($_ENV['PANTHEON_ENVIRONMENT'])) {
-  $settings['config_readonly'] = TRUE;
+//  $settings['config_readonly'] = TRUE;
 } 
 else {
   $config['config_split.config_split.local_development']['status'] = TRUE;
