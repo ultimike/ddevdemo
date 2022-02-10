d8454d70b0116194eae97ed6b7256b1f54c4ddee
Michael Anello <manello@gmail.com>
2022-02-10 11:58:49 -1000

    Removed obsolete Gitpod stuff, part 2.

--- .gitpod/env-setup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-if [ -n "$DEBUG_DRUPALPOD" ]; then
-    set -x
-fi
-
-# Create a phpstorm command
-sudo cp "${GITPOD_REPO_ROOT}"/.gitpod/phpstorm.template.sh /usr/local/bin/phpstorm
